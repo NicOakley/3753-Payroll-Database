a43d1bf7a772ca02dcd5ea9ec2dd5b7e8c071f84
NicOakley <nicholas.woakley@gmail.com>
2022-04-07 21:54:41 -0300

    Created and filled tables in payroll.sql

--- Backend/payroll.sql
+++ Backend/payroll.sql
@@ -33,7 +33,7 @@ GRANT ALL PRIVILEGES ON `payroll`.* TO 'payroll_admin'@'localhost';
 DROP TABLE IF EXISTS `login`;
 
 CREATE TABLE `login` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `login_id` int(11) NOT NULL AUTO_INCREMENT,
     `username` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
     `role` varchar(255) NOT NULL,
@@ -41,19 +41,38 @@ CREATE TABLE `login` (
     UNIQUE KEY `username` (`username`)
 );
 
-CREATE TABLE `Employee` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `employee` (
+    `employee_id` int(11) NOT NULL AUTO_INCREMENT,
     `first_name` varchar(255) NOT NULL,
     `last_name` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL,
     `phone` varchar(255) NOT NULL,
     `hire_date` date NOT NULL,
     `job_title` varchar(255) NOT NULL,
+    `salary` int(11) NOT NULL,
     `hours` int(11) NOT NULL,
     `city` varchar(255) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
+CREATE TABLE `pay` (
+    `pay_id` int(11) NOT NULL AUTO_INCREMENT,
+    `amount` int(11) NOT NULL,
+    `date` date NOT NULL,
+    PRIMARY KEY (`pay_id`),
+    FOREIGN KEY (`employee_id`) REFERENCES `employee`(`employee_id`)
+);
+
+
 INSERT INTO `login` (`id`,`username`, `password`, `role`) VALUES
-(1, 'admin', 'admin', 'admin'),);
-(2, 'user', 'user');
+(1, 'admin', 'admin', 'admin'),
+(2, 'user', 'user', 'user');
+
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `phone`, `hire_date`, `job_title`, `salary`, `hours`, `city`) VALUES
+(1, 'John', 'Doe', 'aaa@bbb.ca', '111-111-1111', '2017-01-01', 'Manager', 100000, 40, 'Toronto'),
+(2, 'Walter', 'White', 'bbb@ccc.ca', '222-222-2222', '2017-01-01', 'Cook', 50000, 40, 'New Mexico');
+
+INSERT INTO `pay` (`employee_id`,`id`, `amount`, `date`) VALUES
+(1, 1, 100, '2017-01-01'),
+(1, 2, 200, '2017-01-08'),
+(2, 3, 300, '2017-01-01');
